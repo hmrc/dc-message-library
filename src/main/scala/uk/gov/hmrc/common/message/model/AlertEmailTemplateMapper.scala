431c060264db69da1290e9540c091298a5f72f1a
satyajosyula2020 <satya.josyula@digital.hmrc.gov.uk>
2023-05-30 11:05:13 +0100

    Fixed the scala style error

--- src/main/scala/uk/gov/hmrc/common/message/model/AlertEmailTemplateMapper.scala
+++ src/main/scala/uk/gov/hmrc/common/message/model/AlertEmailTemplateMapper.scala
@@ -59,9 +59,9 @@ trait AlertEmailTemplateMapper {
 
   private def getItsaTemplateId(formId: String): String =
     itsaTemplates.toList.find(r => r._1.equals(formId.toLowerCase)) match {
-      case Some((_, templateId))       => templateId
-      case _ if formId.endsWith("_cy") => "new_message_alert_itsa_cy"
-      case _                           => "new_message_alert_itsa"
+      case Some((_, templateId))          => templateId
+      case None if formId.endsWith("_cy") => "new_message_alert_itsa_cy"
+      case _                              => "new_message_alert_itsa"
     }
 
   lazy val itsaTemplates = Map(
