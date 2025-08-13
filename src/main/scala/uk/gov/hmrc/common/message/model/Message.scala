9f9fcd2f600be3b4bf9c2580dbdc3d3c9db822ab
Kapil Sanghvi <22366606+kjainsc@users.noreply.github.com>
2025-08-13 17:06:34 +0100

    DC-7696 Update models

--- src/main/scala/uk/gov/hmrc/common/message/model/Message.scala
+++ src/main/scala/uk/gov/hmrc/common/message/model/Message.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024 HM Revenue & Customs
+ * Copyright 2025 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -268,27 +268,4 @@ object MailgunStatus {
     override def writes(status: MailgunStatus): JsValue = JsString(status.name)
   }
 
-}
-
-enum LifecycleStatusType(val entryName: String) {
-  case Submitted extends LifecycleStatusType("SUBMITTED")
-  case SubmissionFailed extends LifecycleStatusType("SUBMISSION_FAILED")
-  case Delivered extends LifecycleStatusType("DELIVERED")
-  case DeliveryFailed extends LifecycleStatusType("DELIVERY_FAILED")
-  case Responded extends LifecycleStatusType("RESPONDED")
-}
-
-object LifecycleStatusType {
-  implicit val format: Format[LifecycleStatusType] = new Format[LifecycleStatusType] {
-    def reads(json: JsValue): JsResult[LifecycleStatusType] = json match {
-      case JsString("SUBMITTED")         => JsSuccess(Submitted)
-      case JsString("SUBMISSION_FAILED") => JsSuccess(SubmissionFailed)
-      case JsString("DELIVERED")         => JsSuccess(Delivered)
-      case JsString("DELIVERY_FAILED")   => JsSuccess(DeliveryFailed)
-      case JsString("RESPONDED")         => JsSuccess(Responded)
-      case _                             => JsError("Invalid Lifecycle Status Type")
-    }
-
-    def writes(status: LifecycleStatusType): JsValue = JsString(status.entryName)
-  }
 }
